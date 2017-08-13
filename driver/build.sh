88b0cdfc6a6fe1c86ea82885b4c01c97ea0ba0a4
Yano Motoharu <motoharu.gosuto@gmail.com>
2017-08-13 14:05:32 +0300

    refine build script

--- driver/build.sh
+++ driver/build.sh
@@ -1,13 +1,14 @@
 rm extra -r
+mkdir extra
 vita-libs-gen -c extra.yml extra
 cd extra
 cmake .
 make
 cd ..
 
+rm build -r
+mkdir build
 cd build
-make clean
-rm CMakeCache.txt
 cmake ../
 make
 make install
