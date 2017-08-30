ca29593b2e39088c739af12db30e72afe4acebc7
Yano Motoharu <motoharu.gosuto@gmail.com>
2017-08-31 01:03:42 +0300

    disable log in functions

--- driver/functions.c
+++ driver/functions.c
@@ -2,6 +2,7 @@
 
 #include "hook_ids.h"
 #include "global_log.h"
+#include "defines.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -18,52 +19,72 @@ int initialize_functions()
   int res = module_get_export_func(KERNEL_PID, "SceKernelThreadMgr", SceThreadmgrForDriver_NID, 0xDB6CD34A, (uintptr_t*)&sceKernelCreateCondForDriver);
   if(res < 0)
   {
+    #ifdef ENABLE_DEBUG_LOG
     snprintf(sprintfBuffer, 256, "failed to set sceKernelCreateCondForDriver : %x\n", res);
     FILE_GLOBAL_WRITE_LEN(sprintfBuffer);
+    #endif
     return -1;
   }
 
+  #ifdef ENABLE_DEBUG_LOG
   FILE_GLOBAL_WRITE_LEN("set sceKernelCreateCondForDriver\n");
+  #endif
 
   res = module_get_export_func(KERNEL_PID, "SceKernelThreadMgr", SceThreadmgrForDriver_NID, 0xAEE0D27C, (uintptr_t*)&sceKernelDeleteCondForDriver);
   if(res < 0)
   {
+    #ifdef ENABLE_DEBUG_LOG
     snprintf(sprintfBuffer, 256, "failed to set sceKernelDeleteCondForDriver : %x\n", res);
     FILE_GLOBAL_WRITE_LEN(sprintfBuffer);
+    #endif
     return -1;
   }
 
+  #ifdef ENABLE_DEBUG_LOG
   FILE_GLOBAL_WRITE_LEN("set sceKernelDeleteCondForDriver\n");
+  #endif
 
   res = module_get_export_func(KERNEL_PID, "SceKernelThreadMgr", SceThreadmgrForDriver_NID, 0xCC7E027D, (uintptr_t*)&sceKernelWaitCondForDriver);
   if(res < 0)
   {
+    #ifdef ENABLE_DEBUG_LOG
     snprintf(sprintfBuffer, 256, "failed to set sceKernelWaitCondForDriver : %x\n", res);
     FILE_GLOBAL_WRITE_LEN(sprintfBuffer);
+    #endif
     return -1;
   }
 
+  #ifdef ENABLE_DEBUG_LOG
   FILE_GLOBAL_WRITE_LEN("set sceKernelWaitCondForDriver\n");
+  #endif
 
   res = module_get_export_func(KERNEL_PID, "SceKernelThreadMgr", SceThreadmgrForDriver_NID, 0xAC616150, (uintptr_t*)&sceKernelSignalCondForDriver);
   if(res < 0)
   {
+    #ifdef ENABLE_DEBUG_LOG
     snprintf(sprintfBuffer, 256, "failed to set sceKernelSignalCondForDriver : %x\n", res);
     FILE_GLOBAL_WRITE_LEN(sprintfBuffer);
+    #endif
     return -1;
   }
 
+  #ifdef ENABLE_DEBUG_LOG
   FILE_GLOBAL_WRITE_LEN("set sceKernelSignalCondForDriver\n");
+  #endif
 
   res = module_get_export_func(KERNEL_PID, "SceSysmem", SceKernelUtilsForDriver_NID, 0x87DC7F2F, (uintptr_t*)&sceKernelSha1DigestForDriver);
   if(res < 0)
   {
+    #ifdef ENABLE_DEBUG_LOG
     snprintf(sprintfBuffer, 256, "failed to set sceKernelSha1DigestForDriver : %x\n", res);
     FILE_GLOBAL_WRITE_LEN(sprintfBuffer);
+    #endif
     return -1;
   }
 
+  #ifdef ENABLE_DEBUG_LOG
   FILE_GLOBAL_WRITE_LEN("set sceKernelSha1DigestForDriver\n");
+  #endif
 
   return 0;
 }
