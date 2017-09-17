3ca29a1cc45c9afb6730d1cad7c4b6a456ba309c
Yano Motoharu <motoharu.gosuto@gmail.com>
2017-09-17 17:05:50 +0300

    add lic header to user app

--- app/src/sfo_utils.c
+++ app/src/sfo_utils.c
@@ -1,3 +1,11 @@
+/* sfo_utils.c
+ *
+ * Copyright (C) 2017 Motoharu Gosuto
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 #include "sfo_utils.h"
 
 #include <stdio.h>
